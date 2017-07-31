a3c2980a78db7c7915ce2aadbe441cc6cc3495d0
SergVL <vlasenko_serg@ukr.net>
2017-07-31 23:33:35 +0300

    Обновил фавикон, добавил лицензии
    1. Подправил фавикон, добавил иконок для мобильных
    2. В футер добавил ссылочку на лицензии и всплывающее окошко лицензий

--- views/footer (3).php
+++ views/footer (3).php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-md-3 footer-left">
                     <h5>Онлайн регистратура в медицинских учреждениях г.&nbspКраматорска</h5>
-					<a data-toggle="modal" href="#LicenseModal" title="Лицензии">license</a>
+                    <a data-toggle="modal" href="#LicenseModal" title="Лицензии">license</a>
                 </div>
                     <?php require("../views/license_form.php");?>
                 <div class="col-md-3 footer-middle-left">
@@ -41,6 +41,40 @@
                 </div>
                 </div>
                 </div>
+            
         </footer>
+
+        <!-- <footer class="footer-center">
+            <div class="panel panel-default">
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-md-5 footer-left">
+                            <h5>Онлайн регистратура в медицинских учреждениях г. Краматорска</h5>
+                        </div>
+                        <div class="col-md-3 footer-middle-left">
+                            <h5>Разработано при поддержке</h5>
+                            <a href="https://brainbasket.org/ru/technology-nation-3/">
+                                <img src="https://brainbasket.org/wp-content/uploads/logo-2.png" style="width: 200px; height: 42px;" alt="BrainBasket Foundation">
+                            </a>
+                        </div>
+                        <div class="col-md-2 footer-middle-right">
+                            <h5>Новости проекта</h5>
+                            <a href="#">
+                                <i class="fa fa-facebook-official" style="font-size:42px"></i>
+                            </a>
+                            &nbsp
+                            <a href="#">
+                                <i class="fa fa-twitter" style="font-size:42px"></i>
+                            </a>
+                        </div>
+                        <div class="col-md-2 footer-right">
+                            <p>Свяжитесь с нами</p>
+                            <p><span class="glyphicon glyphicon-earphone">:&nbsp</span><b>+38-050-878 36 96</b></p>
+                            <p><span class="glyphicon glyphicon-envelope">:&nbsp</span>bro@bro.ua</b></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer> -->
     </body>
 </html>
---
+++ views/license_form.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="LicenseModal" data-keyboard="false" data-backdrop="static" tabindex="-1" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <center><h4 id="modaltitle" class="modal-title">Лицензии</h4></center>
+            </div>
+            <div class="modal-body">
+                <div id="modalcontent2">
+                    <div id="license" class="brd">
+                    	<pt>При разработке сайта были использованы учебные материалы из курса "CS50, Harvard University's".</p>
+                        <p>The MIT License (MIT):</p>
+                        <p>библиотека Client-side JavaScript PDF generation for everyone. https://parall.ax/products/jspdf Copyright (c) 2010-2016 James Hall, https://github.com/MrRio/jsPDF</p>
+                        <div text-align=left>
+                            <p title="Данная лицензия разрешает лицам, получившим копию данного программного обеспечения и сопутствующей документации (в дальнейшем именуемыми «Программное Обеспечение»), безвозмездно использовать Программное Обеспечение без ограничений, включая неограниченное право на использование, копирование, изменение, слияние, публикацию, распространение, сублицензирование и/или продажу копий Программного Обеспечения, а также лицам, которым предоставляется данное Программное Обеспечение, при соблюдении следующих условий:">Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+                            <p title="Указанное выше уведомление об авторском праве и данные условия должны быть включены во все копии или значимые части данного Программного Обеспечения.">The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+                            <p title="ДАННОЕ ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ ПРЕДОСТАВЛЯЕТСЯ «КАК ЕСТЬ», БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ, ЯВНО ВЫРАЖЕННЫХ ИЛИ ПОДРАЗУМЕВАЕМЫХ, ВКЛЮЧАЯ ГАРАНТИИ ТОВАРНОЙ ПРИГОДНОСТИ, СООТВЕТСТВИЯ ПО ЕГО КОНКРЕТНОМУ НАЗНАЧЕНИЮ И ОТСУТСТВИЯ НАРУШЕНИЙ, НО НЕ ОГРАНИЧИВАЯСЬ ИМИ. НИ В КАКОМ СЛУЧАЕ АВТОРЫ ИЛИ ПРАВООБЛАДАТЕЛИ НЕ НЕСУТ ОТВЕТСТВЕННОСТИ ПО КАКИМ-ЛИБО ИСКАМ, ЗА УЩЕРБ ИЛИ ПО ИНЫМ ТРЕБОВАНИЯМ, В ТОМ ЧИСЛЕ, ПРИ ДЕЙСТВИИ КОНТРАКТА, ДЕЛИКТЕ ИЛИ ИНОЙ СИТУАЦИИ, ВОЗНИКШИМ ИЗ-ЗА ИСПОЛЬЗОВАНИЯ ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ ИЛИ ИНЫХ ДЕЙСТВИЙ С ПРОГРАММНЫМ ОБЕСПЕЧЕНИЕМ.">THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+                        </div
+                        >
+                    </div>    
+                </div>
+            </div>
+        </div> <!-- /.modal-content -->
+    </div> <!-- /.modal-dialog -->
+</div> <!-- /.modal -->
+        
+        
